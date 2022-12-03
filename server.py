88046964059d080e90aecb9aef0eadc11c837107
Harsh Mishra <hmisraji07@gmail.com>
2022-12-03 19:27:28 +0530

    Init Commit

---
+++ server.py
@@ -0,0 +1,44 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+
+
+def createJWT(*args, **kwargs):
+    pass
+
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+
+    cursor = mysql.connection.cursorsor()
+    result_set = cursor.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+
+    if result_set > 0:
+        user_row = cursor.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "invalid credentials", 401
+
+
